b80a508421db7b8fbae8592f3a9d1330e150e640
kevinhtran <kevdeveloper7@gmail.com>
2021-05-06 15:46:18 -0700

    article background color, stock image, title, and subtitle

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -16,7 +16,26 @@ const query = graphql`
   }
 `
 const Hero = () => {
-  return <h2>hero component</h2>
+  const {
+    file: {
+      childImageSharp: { fluid },
+    },
+  } = useStaticQuery(query)
+  return (
+    <header className="hero">
+      <div className="section-center hero-center">
+        {/* This will be the left site where you have the title, subtitles, contact button, buttons to social media pages */}
+        <article clasName="hero-info">
+          <div>
+            <div className="underline"></div>
+            <h1>I'm Kevin.</h1>
+            <h4>Full-Stack Developer</h4>
+          </div>
+        </article>
+        <Image fluid={fluid} className="hero-img" />
+      </div>
+    </header>
+  )
 }
 
 export default Hero
