f487a9dc2764822b929c7e8dbe48cdee24dfeba3
kevinhtran <kevdeveloper7@gmail.com>
2021-05-13 02:40:04 -0700

    worked on functionality of jobs component

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -22,6 +22,35 @@ const query = graphql`
 `
 
 const Jobs = () => {
+  // in order to use the query variable:
+  // you have to use the useStaticQuery hook
+  // you create a variable with the useStaticQuery Hook and pass in the query variable from above the component
+  const data = useStaticQuery(query)
+
+  // now, we can start destructuring
+  // set the const {} object equal to data
+  // within {}, we will pull out from the query variable - allStrapiJobs
+  // since it is an object, I want to pull out nodes and give it an alias - jobs
+  const {
+    allStrapiJobs: { nodes: jobs },
+  } = data
+
+  // functionality:
+  // hard-coded:
+  // when we click on the buttons with the company titles, we just want to grab one item from that array (nodes: jobs)
+  // the item is going to depend on the index as we have sorted in our query
+  // you'll always get a different result depending on the index of the job because this is the hard-coded version
+  // const { company, position, date, description } = jobs[0]
+  // dynamically:
+  // in order to set it dynamically, you would want to use useState hook
+  // useState hook always returns an array
+  // [ value, setValue ] is array destructuring
+  // React.useState(0) - default value of zero
+  // we're using useState hook with a default value and we're getting back two things in our array which are destructured
+  // I can name them whatever I want but in this case, I've named them [value, setValue]
+  // setValue is the function that controls it and value represents whatever I have on React.useState(0) which atm is is 0
+  const [value, setValue] = React.useState(0)
+
   return <h2>jobs component</h2>
 }
 
