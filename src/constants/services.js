2b8d7248fa4db6bff169c228461097a8dc9fe95d
kevinhtran <kevdeveloper7@gmail.com>
2021-05-14 15:28:08 -0700

    comments

--- src/constants/services.js
+++ src/constants/services.js
@@ -1,8 +1,13 @@
 import React from "react"
 import { FaCode, FaSketch, FaAndroid } from "react-icons/fa"
+
+// this is the array of objects that you will be exporting
+
 export default [
+  // whatever is placed over here will render on the screen
   {
     id: 1,
+    // you notice here that you can place your imported icon in here as well as a class for styling it
     icon: <FaCode className="service-icon" />,
     title: "web development",
     text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
