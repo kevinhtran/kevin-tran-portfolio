6d7b34a873b11290d9301b5a1157e18ae604da82
kevinhtran <kevdeveloper7@gmail.com>
2021-05-13 17:38:59 -0700

    created button container with job company titles. used a map function to iterate over jobs array and have each job return a dynamic button with state

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -9,6 +9,7 @@ const query = graphql`
   {
     allStrapiJobs(sort: { fields: strapiId, order: DESC }) {
       nodes {
+        strapiId
         company
         date
         position
@@ -40,7 +41,7 @@ const Jobs = () => {
   // when we click on the buttons with the company titles, we just want to grab one item from that array (nodes: jobs)
   // the item is going to depend on the index as we have sorted in our query
   // you'll always get a different result depending on the index of the job because this is the hard-coded version
-  const { company, position, date, description } = jobs[0]
+  // const { company, position, date, description } = jobs[0]
   // dynamically:
   // in order to set it dynamically, you would want to use useState hook
   // useState hook always returns an array
@@ -50,8 +51,38 @@ const Jobs = () => {
   // I can name them whatever I want but in this case, I've named them [value, setValue]
   // setValue is the function that controls it and value represents whatever I have on React.useState(0) which atm is is 0
   const [value, setValue] = React.useState(0)
+  // now we are taking the current value of the useState and it will dynamically change when setValue is invoked
+  const { company, position, date, description } = jobs[value]
 
-  return <h2>jobs component</h2>
+  return (
+    <section className="section jobs">
+      <Title title="experience" />
+      <div className="jobs-center"></div>
+      {/* btn container */}
+      {/* here we have a div */}
+      {/* we want to iterate over our jobs array and for each and every item, we want to display a button */}
+      <div className="btn-container">
+        {/* using a map function with the parameters of job and the index of it */}
+        {/* and then we want it to return the button with the job title */}
+        {/* key={job.strapiId} helps react identify which items have changed, are
+        added, or are removed. */}
+        {/* {job.company} is the item that will have the company field which each value/button */}
+        {/* the className is dynamic. we set up template strings with the className of job-btn but if the index will match the index/value of the current state (jobs[value]) then it will set this up as an active button using the className */}
+        {jobs.map((job, index) => {
+          return (
+            // ternary: if index is equal to whatever the value is then add also an 'active-btn' class
+            <button
+              key={job.strapiId}
+              className={`job-btn ${index === value && "active-btn"}`}
+            >
+              {job.company}
+            </button>
+          )
+        })}
+      </div>
+      {/* job info */}
+    </section>
+  )
 }
 
 export default Jobs
