6e5c0d67a2882e7375432bd8dfbb4a76b5f89c6b
kevinhtran <kevdeveloper7@gmail.com>
2021-05-16 16:44:02 -0700

    added allStrapiProjects query, added projects component with props, destructured allStrapiProjects object from query so we can use our parameter

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,17 +7,31 @@ import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import Blogs from "../components/Blogs"
 
-export default () => {
+// we have this data prop as a parameter now
+export default ({ data }) => {
+  // destructuring
+  // looking for allStrapiProjects, it is an object and within the object i'm looking for the nodes and give it an alias of projects.
+  const {
+    allStrapiProjects: { nodes: projects },
+  } = data
+
   return (
     <Layout>
       <Hero />
       <Services />
       <Jobs />
+      {/* passing 3 props*/}
+      {/* title - we want our title to show featured projects */}
+      {/* showLink - displays the link if it's there */}
+      <Projects project={projects} title="featured projects" showLink />
     </Layout>
   )
 }
 
-const query = graphql`
+// we place this projects content query here because we just pass this query into a the projects component as a prop
+// later in the projects page, we run the query in our projects component but without the featured filter
+// instead of setting the query in the project, i'll set the query in the pages - index.js where i'll display those projects
+export const query = graphql`
   {
     allStrapiProjects(filter: { featured: { eq: true } }) {
       nodes {
