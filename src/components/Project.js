63f844c64cc53cf94823b8f66d8faef6c852124b
kevinhtran <kevdeveloper7@gmail.com>
2021-05-16 22:18:09 -0700

    created project(singular) component using a destructured list of the query nodes. created article section with fluid project images, information with styled indexing, description, stack tags using map function, and links using react-icons

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,8 +5,40 @@ import React from "react"
 import PropTypes from "prop-types"
 import Image from "gatsby-image"
 import { FaGithubSquare, FaShareSquare } from "react-icons/fa"
-const Project = () => {
-  return <h2>project copmonent</h2>
+// destructured all these things from the query
+const Project = ({ description, title, github, stack, url, image, index }) => {
+  return (
+    <article className="project">
+      {/* we are looking for the fluid image, so we have to pass in fluid prop with the correct path to fluid */}
+      {/* these will be the project images inside of Strapi*/}
+      <Image fluid={image.childImageSharp.fluid} className="project-img" />
+      {/* this is the project info */}
+
+      <div className="project-info">
+        {/* this is the project number which is displayed through the index */}
+        <span className="project-number">0{index + 1}</span>
+        {/* this is the title with the title prop passed in */}
+        <h3>{title}</h3>
+        {/* this is the description with the description prop passed in */}
+        <p className="project-desc">{description}</p>
+        {/* within this stack, we'll iterate over it */}
+        <div className="project-stack">
+          {stack.map(item => {
+            return <span key={item.id}>{item.title}</span>
+          })}
+        </div>
+        {/* these are the two href links using react-icons */}
+        <div className="project-links">
+          <a href={github}>
+            <FaGithubSquare className="project-icon" />
+          </a>
+          <a href={url}>
+            <FaShareSquare className="project-icon" />
+          </a>
+        </div>
+      </div>
+    </article>
+  )
 }
 
 Project.propTypes = {}
