e52047d053c0f9c93e44ddf1384d954cba3f98c1
kevinhtran <kevdeveloper7@gmail.com>
2021-05-17 23:46:25 -0700

    created sidebar component without functionality yet

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,6 +7,8 @@ const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
+      {/* doesn't really matter where you place the sidebar because it is using position-fixed */}
+      <Sidebar />
       {/* contents of the page */}
       {children}
       <Footer />
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,8 +2,24 @@ import React from "react"
 import Links from "../constants/links"
 import SocialLinks from "../constants/socialLinks"
 import { FaTimes } from "react-icons/fa"
+
 const Sidebar = () => {
-  return <h2>sidebar component</h2>
+  const isOpen = false
+  // we use template strings for the aside tag because we toggle one class (show sidebar). We have two classes and two states - open and close
+  // show sidebar will be in between those two classes
+  return (
+    // show-sidebar is the class responsible for toggling
+    // styleClass is a specific prop responsible for passing in the css class
+    <aside className={`sidebar ${isOpen ? "show-sidebar" : ""}`}>
+      <button className="closed-btn">
+        <FaTimes />
+      </button>
+      <div className="side-container">
+        <Links styleClass="sidebar-links" />
+        <SocialLinks styleClass="sidebar-icons" />
+      </div>
+    </aside>
+  )
 }
 
 export default Sidebar
