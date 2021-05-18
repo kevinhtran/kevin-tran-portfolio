deb61250d8fcc9590eb9976db871507a7c323a3d
kevinhtran <kevdeveloper7@gmail.com>
2021-05-17 17:47:46 -0700

    passed in props from index.js as parameters

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -2,7 +2,7 @@ import React from "react"
 import Title from "./Title"
 import Blog from "./Blog"
 import { Link } from "gatsby"
-export const Blogs = () => {
+export const Blogs = ({ blogs, title, showLink }) => {
   return <h2>blog list section</h2>
 }
 export default Blogs
