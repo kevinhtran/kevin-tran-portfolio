44774677d619aafe6dbd3f7d9524e5f81ab0b990
kevinhtran <kevdeveloper7@gmail.com>
2021-05-24 18:40:31 -0700

    fixed typo with footer. copyright footer now showing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,15 @@
 import React from "react"
 import SocialLinks from "../constants/socialLinks"
+
 const Footer = () => {
   return (
     <footer className="footer">
       <div>
-        <SocialLinks styleClass="footer-links">
-          <h4>
-            copyright&copy;{new Date().getFullYear()}
-            <span>Kevin Tran</span> all rights reserved
-          </h4>
-        </SocialLinks>
+        <SocialLinks styleClass="footer-links"></SocialLinks>
+        <h4>
+          &copy;{new Date().getFullYear()}
+          <span>Kevin Tran</span> all rights reserved
+        </h4>
       </div>
     </footer>
   )
